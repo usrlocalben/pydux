ed1eaa1137d5b494afffef5c8316a6249012dedd
Benjamin Yates <benjamin@rqdq.com>
2016-10-15 15:35:53 -0400

    re-export core methods from __init__.py

--- pydux/__init__.py
+++ pydux/__init__.py
@@ -1,1 +1,7 @@
+from __future__ import absolute_import
+
+from .apply_middleware import apply_middleware
+from .combine_reducers import combine_reducers
+from .create_store import create_store
+
 __version__ = '0.1.0'
