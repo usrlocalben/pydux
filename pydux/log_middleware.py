b99e083cc8526c375cd23e70d6360c18826974cc
chriskiehl <audionautic@gmail.com>
2017-03-21 21:55:33 -0700

    wrap print in parens for python3 compatibility

--- pydux/log_middleware.py
+++ pydux/log_middleware.py
@@ -7,7 +7,7 @@ def log_middleware(store):
     """log all actions to console as they are dispatched"""
     def wrapper(next_):
         def log_dispatch(action):
-            print 'Dispatch Action:', action
+            print('Dispatch Action:', action)
             return next_(action)
         return log_dispatch
     return wrapper
